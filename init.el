6a777a416c7585138eadc011e13af44b8005e9f6
Taian Su <me@taian.su>
2014-05-14 02:58:57 +0800

    add uniquify-buffer-name-style, important-bell-only, ido-mode, powerline, rainbow-delimiters and grep-a-lot

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 (package-initialize)
+(require 'powerline)
+(powerline-default-theme)
+(require 'grep-a-lot)
+(grep-a-lot-setup-keys)
+(require 'rainbow-delimiters)
+
+;; ==============================
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -10,16 +17,31 @@
  '(custom-safe-themes
    (quote
     ("e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default)))
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
  '(menu-bar-mode nil)
- '(nyan-wavy-trail t)
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "http://melpa.milkbox.net/packages/"))))
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
+
+;; ==============================
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 160 :width normal :foundry "nil" :family "Source Code Pro")))))
+
+;; ==============================
+
+(defun important-bell-only ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'important-bell-only)
